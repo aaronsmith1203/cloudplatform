2acb21d509645ea5dc37d16bfc27ed6383e4f73d
Aaron Smith <email@aaronsmith.email>
2022-03-07 11:16:13 +0000

    feat - added tenantName attribute
    Added tenantName attribute to the Account class entity, along with
    getters and setters.
    
    CP-33 #completed

--- src/main/java/info/aaronsmith/demo/cloudplatform/accounts/Account.java
+++ src/main/java/info/aaronsmith/demo/cloudplatform/accounts/Account.java
@@ -13,6 +13,22 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String tenantName;
 	
 	public Account() { }
+	public Account(String tenantName) {
+		this.tenantName = tenantName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTenantName() {
+		return tenantName;
+	}
+	public void setTenantName(String tenantName) {
+		this.tenantName = tenantName;
+	}
 }
