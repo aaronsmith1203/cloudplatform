d661c1fe34779a4fd8a7d837f7426be2753359c3
Aaron Smith <email@aaronsmith.email>
2022-03-07 12:59:06 +0000

    feat - added crud functionality for account entity
    - Added CRUD methods to accountService
    - Added exception AccountNotFound which is throw correctly when
      attempting to get, update, or delete an account that does not exist
    - Added controller endpoints for common CRUD operations:
      - createAccount
      - getAccounts (all)
      - getAccount
      - updateAccount
      - deleteAccount
    
    CP-34 #done

--- src/main/java/info/aaronsmith/demo/cloudplatform/accounts/AccountService.java
+++ src/main/java/info/aaronsmith/demo/cloudplatform/accounts/AccountService.java
@@ -1,5 +1,7 @@
 package info.aaronsmith.demo.cloudplatform.accounts;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,54 @@ public class AccountService {
 	@Autowired
 	public AccountService(AccountRepo repo) {
 		this.repo = repo;
+	}
+	
+	// CREATE
+	public Account createAccount(Account account) {
+		return repo.save(account);
+	}
+	
+	// READ
+	public List<Account> getAccount() {
+		return repo.findAll();
+	}
+	
+	// READ
+	public Account getAccount(Integer id) {
+		try {
+			return repo.findById(id).get();
+		}
+		catch (Exception e) {
+			throw new AccountNotFoundException(id);
+		}
+	}
+	
+	// UPDATE
+	public Account updateAccount(Integer id, Account account) {
+		try {
+			// get existing account from database
+			Account foundAccount = getAccount(id);
+			
+			// update Account attributes
+			foundAccount.setTenantName(account.getTenantName());
+			
+			// save Account back to database
+			return repo.save(foundAccount);
+		}
+		catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	// DELETE
+	public void deleteAccount(Integer id) {
+		try {
+			// get account object to verify it exists in database
+			getAccount(id);
+			repo.deleteById(id);
+		}
+		catch (Exception e) {
+			throw e;
+		}
 	}
 }
--- src/main/java/info/aaronsmith/demo/cloudplatform/api/Controller.java
+++ src/main/java/info/aaronsmith/demo/cloudplatform/api/Controller.java
@@ -1,8 +1,69 @@
 package info.aaronsmith.demo.cloudplatform.api;
 
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import info.aaronsmith.demo.cloudplatform.accounts.Account;
+import info.aaronsmith.demo.cloudplatform.accounts.AccountNotFoundException;
+import info.aaronsmith.demo.cloudplatform.accounts.AccountService;
 
 @RestController
 public class Controller {
 
+	private AccountService as;
+	
+	@Autowired
+	public Controller(AccountService accountService) {
+		this.as = accountService;
+	}
+	
+	// CREATE
+	@PostMapping("/createAccount")
+	public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+		Account newAccount = as.createAccount(account);
+		return new ResponseEntity<Account>(newAccount, HttpStatus.CREATED);
+	}
+	
+	// READ - get all accounts
+	@GetMapping("/getAccounts")
+	public ResponseEntity<List<Account>> getAccount() {
+		return new ResponseEntity<List<Account>>(as.getAccount(), HttpStatus.OK);
+	}
+	
+	// READ - get account by id
+	@GetMapping("/getAccount/{id}")
+	public ResponseEntity<Account> getAccount(@PathVariable Integer id) {
+		return new ResponseEntity<Account>(as.getAccount(id), HttpStatus.OK);
+	}
+	
+	// UPDATE
+	@PutMapping("/updateAccount/{id}")
+	public ResponseEntity<Account> updateAccount(@PathVariable Integer id, @RequestBody Account account) {
+		return new ResponseEntity<Account>(as.updateAccount(id, account), HttpStatus.OK);
+	}
+	
+	// DELETE
+	@DeleteMapping("/deleteAccount/{id}")
+	public ResponseEntity<String> deleteAccount(@PathVariable Integer id) {
+		try {
+			as.deleteAccount(id);
+			return new ResponseEntity<String>("Successfully deleted account with id (" + id + ")", HttpStatus.OK);
+		}
+		catch (Exception e) {
+			throw e;
+		}
+	}
+	
 }
