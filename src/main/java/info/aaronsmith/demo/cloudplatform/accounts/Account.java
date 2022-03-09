41db27814b38b318ed862f5bc3c5fba6db28d77a
Aaron Smith <email@aaronsmith.email>
2022-03-09 12:14:57 +0000

    feat - added attributes to Account entity
    - Added attributes to Account class
    - Updated constructors
    - Updated equals() override method
    - Added toString() override method
    - Added hashCode() override method
    
    CP12 CP38

--- src/main/java/info/aaronsmith/demo/cloudplatform/accounts/Account.java
+++ src/main/java/info/aaronsmith/demo/cloudplatform/accounts/Account.java
@@ -1,5 +1,7 @@
 package info.aaronsmith.demo.cloudplatform.accounts;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,39 +18,212 @@ public class Account {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	private String title;
+	
+	@NotNull
+	private String firstName;
+	
+	@NotNull
+	private String lastName;
+	
 	@NotNull
 	@Column(unique = true, nullable = false)
 	private String tenantName;
 	
+	@NotNull
+	private String addressLine1;
+	private String addressLine2;
+	
+	@NotNull
+	private String city;
+	private String county;
+	
+	@NotNull
+	private String postCode;
+	
+	// need to validate this
+	private String telephoneNumber;
+	
+	@NotNull
+	// need to validate this
+	private String emailAddress;
+	
 	public Account() { }
 	
-	public Account(Integer id, String tenantName) {
+	public Account(
+			Integer id,
+			String title,
+			String firstName,
+			String lastName,
+			String tenantName,
+			String addressLine1,
+			String addressLine2,
+			String city,
+			String county,
+			String postCode,
+			String telephoneNumber,
+			String emailAddress
+	) {
 		this.id = id;
+		this.title = title;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.tenantName = tenantName;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.county = county;
+		this.postCode = postCode;
+		this.telephoneNumber = telephoneNumber;
+		this.emailAddress = emailAddress;
 	}
 	
-	public Account(String tenantName) {
+	public Account(
+			String title,
+			String firstName,
+			String lastName,
+			String tenantName,
+			String addressLine1,
+			String addressLine2,
+			String city,
+			String county,
+			String postCode,
+			String telephoneNumber,
+			String emailAddress
+	) {
+		this.title = title;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.tenantName = tenantName;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.county = county;
+		this.postCode = postCode;
+		this.telephoneNumber = telephoneNumber;
+		this.emailAddress = emailAddress;
 	}
 
 	public Integer getId() {
 		return id;
 	}
+	
+	public String getTitle() {
+		return title;
+	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 	public String getTenantName() {
 		return tenantName;
 	}
-
+	
 	public void setTenantName(String tenantName) {
 		this.tenantName = tenantName;
 	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
 	
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-	    if (o == null || getClass() != o.getClass()) return false;
-	    Account that = (Account) o;
-	    return id.equals(that.getId()) &&
-			   tenantName.equals(that.getTenantName());
+	public String toString() {
+		return "Account [id=" + id + ", title=" + title + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", tenantName=" + tenantName + ", addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2
+				+ ", city=" + city + ", county=" + county + ", postCode=" + postCode + ", telephoneNumber="
+				+ telephoneNumber + ", emailAddress=" + emailAddress + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(addressLine1, addressLine2, city, county, emailAddress, firstName, id, lastName, postCode,
+				telephoneNumber, tenantName, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Account)) {
+			return false;
+		}
+		Account other = (Account) obj;
+		return Objects.equals(addressLine1, other.addressLine1) && Objects.equals(addressLine2, other.addressLine2)
+				&& Objects.equals(city, other.city) && Objects.equals(county, other.county)
+				&& Objects.equals(emailAddress, other.emailAddress) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(postCode, other.postCode) && Objects.equals(telephoneNumber, other.telephoneNumber)
+				&& Objects.equals(tenantName, other.tenantName) && Objects.equals(title, other.title);
+	}	
 }
